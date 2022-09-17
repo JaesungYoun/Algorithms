a009425695126ac463153a3acc727a48c5cef8ec
younjaesung <jayjoy05@khu.ac.kr>
2022-09-17 19:01:13 +0900

    algorithms

--- Greedy/18310.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-N = int(sys.stdin.readline())
-
-pos = sorted(list(map(int,sys.stdin.readline().split())))
-
-print(pos[(N-1)//2])
-
--- 구현/9659.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-N = int(sys.stdin.readline())
-
-
-if N % 2 == 1:
-    print("SK")
-else:
-    print("CY")
