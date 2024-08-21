8858136711c84dc86456648c54e7de6dceadfc89
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-21 17:12:38 +0900

    [DP] 2024-08-20

--- Programmers/Python/DP/멀리뛰기.py
+++ Programmers/Python/DP/멀리뛰기.py
@@ -1,17 +1,13 @@
 def solution(n):
-    answer = 0
-    
-    if n== 1:
-        return 1
-    
-    dp = [0] * (n+1)
-    
+    dp = [0] * (n + 1)
+
     dp[1] = 1
+    if n == 1:
+        return dp[1] % 1234567
+
     dp[2] = 2
-    if n >= 3:
-        for i in range(3,n+1):
-            dp[i] = (dp[i-1] + dp[i-2]) % 1234567
-    
-    
-    answer = dp[n]
-    return answer
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 2] + dp[i - 1]
+
+    return dp[n] % 1234567
